e5dd1416c79ceb4a3878f844a52181df6e35751f
remygin <remygin@gmail.com>
2016-11-01 20:22:44 +0300

    display for modals after click

--- source/js/main.js
+++ source/js/main.js
@@ -25,11 +25,46 @@ var editSocial = (function(){
 
 $(document).ready(function(){
     editSocial().init();
+    ////////////////////////
+    // BUTTON EDIT INFO USER
+    ////////////////////////
     $(".button_edit-info").on("click", function(){
         $(".changes").addClass("changes-active");
-    })
+    });
     $(".close__header__edit").on("click", function(){
         $(".changes").removeClass("changes-active");
-    })
+    });
+
+
+    ////////////////////
+    // BUTTON CLOSE MODAL WINDOW
+    ////////////////////
+    $(".modal-btn__close").on("click", function (e) {
+        e.preventDefault();
+        $(".modal").css({display: "none"});
+        document.onmousewheel = function (e) {
+        }
+    });
+    ////////////////////
+    // BUTTON ADD ALBUMS
+    ////////////////////
+    $(".button_add").on("click", function (e) {
+        e.preventDefault();
+        $(".add-album, .add-photo").css({display: "flex"});
+        document.onmousewheel = function (e) {
+            e.preventDefault();
+        }
+    });
+    ////////////////////
+    // BUTTON ADD ALBUMS
+    ////////////////////
+    $(".my-albums-edit, .edit-post").on("click", function (e) {
+        e.preventDefault();
+        $(".edit-photo").css({display: "flex"});
+        document.onmousewheel = function (e) {
+            e.preventDefault();
+        }
+    });
+
 });
 
