2eb6c7413f167852f8f3b4229fd60f1f8d1f9b68
Maxim Matyuschenko <matyuschenko@gmail.com>
2016-11-08 23:51:42 +0300

    add date, photopath and albumID fields to data send with photo-edit

--- source/js/modal_edit-photo.js
+++ source/js/modal_edit-photo.js
@@ -12,7 +12,11 @@ $(window).on('load', function() {
             sendForm = function () {
                 var form = $('#form__edit-photo'),
                     inputs = form.find('input, textarea'),
-                    res = {};
+                    res = {
+                        date: new Date(),
+                        photoPath: 'test',
+                        album_ID: 'test'
+                    };
                 inputs.each(function () {
                     res[this.name] = this.value;
                 });
